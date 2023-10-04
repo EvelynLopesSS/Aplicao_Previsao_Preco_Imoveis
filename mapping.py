009dc1f892995b53a0f1c66a854bcadce166b9f8
Evelyn Lopes <113462824+EvelynLopesSS@users.noreply.github.com>
2023-10-03 21:30:11 -0300

    Create mapping.py

---
+++ mapping.py
@@ -0,0 +1,82 @@
+cidade_mapping = {
+    'João Pessoa': 0,
+    'Cabedelo': 1
+}
+bairro_mapping = {
+    'Jardim Oceania': 0,
+    'Cabo Branco': 1,
+    'Intermares': 2,
+    'Manaíra': 3,
+    'Bessa': 4,
+    'Tambaú': 5,
+    'Bancários': 6,
+    'Altiplano Cabo Branco': 7,
+    'Aeroclube': 8,
+    'Miramar': 9,
+    'Estados': 10,
+    'Praia Formosa': 11,
+    'Camboinha': 12,
+    'Ponta de Campina': 13,
+    'Jardima Cidade Universitária': 14,
+    'Poço': 15,
+    'Brisamar': 16,
+    'Tambauzinho': 17,
+    'Expedicionários': 18,
+    'Portal do Sol': 19,
+    'Cidade dos Colibris': 20,
+    'João Agripino': 21,
+    'Jaguaribe': 22,
+    'Centro': 23,
+    'Cristo Redentor': 24,
+    'Areia Dourada': 25,
+    'Jardim São Paulo': 26,
+    'Parque Verde': 27,
+    'Torre': 28,
+    'Treze de Maio': 29,
+    'Ponta do Seixas': 30,
+    'Anatólia': 31,
+    'José Américo de Almeida': 32,
+    'Pedro Gondim': 33,
+    'Tambiá': 34,
+    'Planalto Boa Esperança': 35,
+    'Gramame': 36,
+    'Jardim Veneza': 37,
+    'Ernesto Geisel': 38,
+    'Água Fria': 39,
+    'Jardim Luna': 40,
+    'Valentina de Figueiredo': 41,
+    'Jardim Brasília': 42,
+    'Castelo Branco': 43,
+    'Cuiá': 44,
+    'João Paulo II': 45,
+    'Formosa': 46,
+    'Ipês': 47,
+    'Oitizeiro': 48
+}
+andar_mapping = {
+    '1º ao 5º': 0,
+    'Térreo': 1,
+    '6º ao 10º': 2,
+    '11º ao 15º': 3,
+    '16º ao 20º ': 4,
+    'Acima do 20º': 5
+}
+
+status_mapping = {
+    'Em construção':0,
+    'Avulso': 1,
+    'Pronto': 2
+}
+
+tipo_imovel_mapping = {
+    'Apartamento': 0,
+    'Flat': 1,
+    'Sala Comercial': 2,
+    'Cobertura Duplex': 3,
+    'Casa': 4,
+    'Terreno': 5,
+    'Loja': 6,
+    'Casa Condomínio': 7,
+    'Cobertura': 8,
+    'Terreno Condomínio': 9
+}
